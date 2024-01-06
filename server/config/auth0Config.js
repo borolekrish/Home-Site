37250e9f5a487277f5fed7199e3f6227b3757db3
borolekrish <borolekrish1@gmail.com>
2024-01-06 23:17:29 +0530

    dssd

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,8 +1,8 @@
-import {auth} from 'express-oauth2-jwt-bearer'
+import { auth } from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
     audience: "http://localhost:8000",
-    issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
+    issuerBaseURL: "borolekrish.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
 
